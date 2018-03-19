0f4735e323467c5b26bd28d181a2eb7228976d27
obophenotype <ontologywizard@example.org>
2018-03-19 07:56:43 -0700

    regenerated

---
+++ java-gen/DiseaseOrPhenotypicFeatureAssociationToLocationAssociation.java
@@ -0,0 +1,293 @@
+import java.util.ArrayList;
+import java.util.List;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyDescription;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
+import org.apache.commons.lang.builder.ToStringBuilder;
+
+
+/**
+ * DiseaseOrPhenotypicFeatureAssociationToLocationAssociation
+ * <p>
+ * An association between either a disease or a phenotypic feature and an anatomical entity, where the disease/feature manifests in that site.
+ * 
+ */
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+    "association_type",
+    "id",
+    "label",
+    "negated",
+    "object",
+    "provided_by",
+    "publications",
+    "qualifiers",
+    "relation",
+    "subject"
+})
+public class DiseaseOrPhenotypicFeatureAssociationToLocationAssociation {
+
+    /**
+     * connects an association to the type of association (e.g. gene to phenotype)
+     * 
+     */
+    @JsonProperty("association_type")
+    @JsonPropertyDescription("connects an association to the type of association (e.g. gene to phenotype)")
+    private String associationType;
+    @JsonProperty("id")
+    private String id;
+    /**
+     * A human-readable name for a thing
+     * 
+     */
+    @JsonProperty("label")
+    @JsonPropertyDescription("A human-readable name for a thing")
+    private String label;
+    /**
+     * if set to true, then the association is negated i.e. is not true
+     * 
+     */
+    @JsonProperty("negated")
+    @JsonPropertyDescription("if set to true, then the association is negated i.e. is not true")
+    private String negated;
+    /**
+     * connects an association to the object of the association. For example, in a gene-to-phenotype association, the gene is subject and phenotype is object.
+     * 
+     */
+    @JsonProperty("object")
+    @JsonPropertyDescription("connects an association to the object of the association. For example, in a gene-to-phenotype association, the gene is subject and phenotype is object.")
+    private String object;
+    /**
+     * connects an association to the agent (person, organization or group) that provided it
+     * 
+     */
+    @JsonProperty("provided_by")
+    @JsonPropertyDescription("connects an association to the agent (person, organization or group) that provided it")
+    private String providedBy;
+    /**
+     * connects an association to publications supporting the association
+     * 
+     */
+    @JsonProperty("publications")
+    @JsonPropertyDescription("connects an association to publications supporting the association")
+    private List<String> publications = new ArrayList<String>();
+    /**
+     * connects an association to qualifiers that modify or qualify the meaning of that association
+     * 
+     */
+    @JsonProperty("qualifiers")
+    @JsonPropertyDescription("connects an association to qualifiers that modify or qualify the meaning of that association")
+    private List<String> qualifiers = new ArrayList<String>();
+    /**
+     * the relationship type by which a subject is connected to an object in an association
+     * 
+     */
+    @JsonProperty("relation")
+    @JsonPropertyDescription("the relationship type by which a subject is connected to an object in an association")
+    private String relation;
+    /**
+     * connects an association to the subject of the association. For example, in a gene-to-phenotype association, the gene is subject and phenotype is object.
+     * 
+     */
+    @JsonProperty("subject")
+    @JsonPropertyDescription("connects an association to the subject of the association. For example, in a gene-to-phenotype association, the gene is subject and phenotype is object.")
+    private String subject;
+
+    /**
+     * connects an association to the type of association (e.g. gene to phenotype)
+     * 
+     */
+    @JsonProperty("association_type")
+    public String getAssociationType() {
+        return associationType;
+    }
+
+    /**
+     * connects an association to the type of association (e.g. gene to phenotype)
+     * 
+     */
+    @JsonProperty("association_type")
+    public void setAssociationType(String associationType) {
+        this.associationType = associationType;
+    }
+
+    @JsonProperty("id")
+    public String getId() {
+        return id;
+    }
+
+    @JsonProperty("id")
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    /**
+     * A human-readable name for a thing
+     * 
+     */
+    @JsonProperty("label")
+    public String getLabel() {
+        return label;
+    }
+
+    /**
+     * A human-readable name for a thing
+     * 
+     */
+    @JsonProperty("label")
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    /**
+     * if set to true, then the association is negated i.e. is not true
+     * 
+     */
+    @JsonProperty("negated")
+    public String getNegated() {
+        return negated;
+    }
+
+    /**
+     * if set to true, then the association is negated i.e. is not true
+     * 
+     */
+    @JsonProperty("negated")
+    public void setNegated(String negated) {
+        this.negated = negated;
+    }
+
+    /**
+     * connects an association to the object of the association. For example, in a gene-to-phenotype association, the gene is subject and phenotype is object.
+     * 
+     */
+    @JsonProperty("object")
+    public String getObject() {
+        return object;
+    }
+
+    /**
+     * connects an association to the object of the association. For example, in a gene-to-phenotype association, the gene is subject and phenotype is object.
+     * 
+     */
+    @JsonProperty("object")
+    public void setObject(String object) {
+        this.object = object;
+    }
+
+    /**
+     * connects an association to the agent (person, organization or group) that provided it
+     * 
+     */
+    @JsonProperty("provided_by")
+    public String getProvidedBy() {
+        return providedBy;
+    }
+
+    /**
+     * connects an association to the agent (person, organization or group) that provided it
+     * 
+     */
+    @JsonProperty("provided_by")
+    public void setProvidedBy(String providedBy) {
+        this.providedBy = providedBy;
+    }
+
+    /**
+     * connects an association to publications supporting the association
+     * 
+     */
+    @JsonProperty("publications")
+    public List<String> getPublications() {
+        return publications;
+    }
+
+    /**
+     * connects an association to publications supporting the association
+     * 
+     */
+    @JsonProperty("publications")
+    public void setPublications(List<String> publications) {
+        this.publications = publications;
+    }
+
+    /**
+     * connects an association to qualifiers that modify or qualify the meaning of that association
+     * 
+     */
+    @JsonProperty("qualifiers")
+    public List<String> getQualifiers() {
+        return qualifiers;
+    }
+
+    /**
+     * connects an association to qualifiers that modify or qualify the meaning of that association
+     * 
+     */
+    @JsonProperty("qualifiers")
+    public void setQualifiers(List<String> qualifiers) {
+        this.qualifiers = qualifiers;
+    }
+
+    /**
+     * the relationship type by which a subject is connected to an object in an association
+     * 
+     */
+    @JsonProperty("relation")
+    public String getRelation() {
+        return relation;
+    }
+
+    /**
+     * the relationship type by which a subject is connected to an object in an association
+     * 
+     */
+    @JsonProperty("relation")
+    public void setRelation(String relation) {
+        this.relation = relation;
+    }
+
+    /**
+     * connects an association to the subject of the association. For example, in a gene-to-phenotype association, the gene is subject and phenotype is object.
+     * 
+     */
+    @JsonProperty("subject")
+    public String getSubject() {
+        return subject;
+    }
+
+    /**
+     * connects an association to the subject of the association. For example, in a gene-to-phenotype association, the gene is subject and phenotype is object.
+     * 
+     */
+    @JsonProperty("subject")
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this).append("associationType", associationType).append("id", id).append("label", label).append("negated", negated).append("object", object).append("providedBy", providedBy).append("publications", publications).append("qualifiers", qualifiers).append("relation", relation).append("subject", subject).toString();
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder().append(providedBy).append(negated).append(subject).append(associationType).append(qualifiers).append(id).append(label).append(object).append(publications).append(relation).toHashCode();
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+        if ((other instanceof DiseaseOrPhenotypicFeatureAssociationToLocationAssociation) == false) {
+            return false;
+        }
+        DiseaseOrPhenotypicFeatureAssociationToLocationAssociation rhs = ((DiseaseOrPhenotypicFeatureAssociationToLocationAssociation) other);
+        return new EqualsBuilder().append(providedBy, rhs.providedBy).append(negated, rhs.negated).append(subject, rhs.subject).append(associationType, rhs.associationType).append(qualifiers, rhs.qualifiers).append(id, rhs.id).append(label, rhs.label).append(object, rhs.object).append(publications, rhs.publications).append(relation, rhs.relation).isEquals();
+    }
+
+}
---
+++ java-gen/RnaProductIsoform.java
@@ -0,0 +1,135 @@
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyDescription;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
+import org.apache.commons.lang.builder.ToStringBuilder;
+
+
+/**
+ * RnaProductIsoform
+ * <p>
+ * Represents a protein that is a specific isoform of the canonical or reference RNA
+ * 
+ */
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+    "has_biological_sequence",
+    "id",
+    "in_taxon",
+    "label"
+})
+public class RnaProductIsoform {
+
+    /**
+     * connects a genomic feature to its sequence
+     * 
+     */
+    @JsonProperty("has_biological_sequence")
+    @JsonPropertyDescription("connects a genomic feature to its sequence")
+    private String hasBiologicalSequence;
+    @JsonProperty("id")
+    private String id;
+    /**
+     * connects a thing to a class representing a taxon
+     * 
+     */
+    @JsonProperty("in_taxon")
+    @JsonPropertyDescription("connects a thing to a class representing a taxon")
+    private String inTaxon;
+    /**
+     * A human-readable name for a thing
+     * 
+     */
+    @JsonProperty("label")
+    @JsonPropertyDescription("A human-readable name for a thing")
+    private String label;
+
+    /**
+     * connects a genomic feature to its sequence
+     * 
+     */
+    @JsonProperty("has_biological_sequence")
+    public String getHasBiologicalSequence() {
+        return hasBiologicalSequence;
+    }
+
+    /**
+     * connects a genomic feature to its sequence
+     * 
+     */
+    @JsonProperty("has_biological_sequence")
+    public void setHasBiologicalSequence(String hasBiologicalSequence) {
+        this.hasBiologicalSequence = hasBiologicalSequence;
+    }
+
+    @JsonProperty("id")
+    public String getId() {
+        return id;
+    }
+
+    @JsonProperty("id")
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    /**
+     * connects a thing to a class representing a taxon
+     * 
+     */
+    @JsonProperty("in_taxon")
+    public String getInTaxon() {
+        return inTaxon;
+    }
+
+    /**
+     * connects a thing to a class representing a taxon
+     * 
+     */
+    @JsonProperty("in_taxon")
+    public void setInTaxon(String inTaxon) {
+        this.inTaxon = inTaxon;
+    }
+
+    /**
+     * A human-readable name for a thing
+     * 
+     */
+    @JsonProperty("label")
+    public String getLabel() {
+        return label;
+    }
+
+    /**
+     * A human-readable name for a thing
+     * 
+     */
+    @JsonProperty("label")
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this).append("hasBiologicalSequence", hasBiologicalSequence).append("id", id).append("inTaxon", inTaxon).append("label", label).toString();
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder().append(hasBiologicalSequence).append(id).append(label).append(inTaxon).toHashCode();
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+        if ((other instanceof RnaProductIsoform) == false) {
+            return false;
+        }
+        RnaProductIsoform rhs = ((RnaProductIsoform) other);
+        return new EqualsBuilder().append(hasBiologicalSequence, rhs.hasBiologicalSequence).append(id, rhs.id).append(label, rhs.label).append(inTaxon, rhs.inTaxon).isEquals();
+    }
+
+}
