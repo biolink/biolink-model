bd4b12d46f311b0cb50fcc7474bf8720122a8f3c
cmungall <cjm@berkeleybop.org>
2018-04-22 17:29:01 -0700

    new classes

---
+++ java-gen/VariantToPhenotypicFeatureAssociation.java
@@ -0,0 +1,397 @@
+import java.util.ArrayList;
+import java.util.List;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyDescription;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
+import org.apache.commons.lang.builder.ToStringBuilder;
+
+
+/**
+ * VariantToPhenotypicFeatureAssociation
+ * <p>
+ * null
+ * 
+ */
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+    "association_type",
+    "frequency_qualifier",
+    "id",
+    "label",
+    "negated",
+    "object",
+    "onset_qualifier",
+    "provided_by",
+    "publications",
+    "qualifiers",
+    "relation",
+    "severity_qualifier",
+    "sex_qualifier",
+    "subject"
+})
+public class VariantToPhenotypicFeatureAssociation {
+
+    /**
+     * connects an association to the type of association (e.g. gene to phenotype)
+     * 
+     */
+    @JsonProperty("association_type")
+    @JsonPropertyDescription("connects an association to the type of association (e.g. gene to phenotype)")
+    private String associationType;
+    /**
+     * a qualifier used in a phenotypic association to state how frequent the phenotype is observed in the subject
+     * 
+     */
+    @JsonProperty("frequency_qualifier")
+    @JsonPropertyDescription("a qualifier used in a phenotypic association to state how frequent the phenotype is observed in the subject")
+    private String frequencyQualifier;
+    @JsonProperty("id")
+    private String id;
+    /**
+     * A human-readable name for a thing
+     * 
+     */
+    @JsonProperty("label")
+    @JsonPropertyDescription("A human-readable name for a thing")
+    private String label;
+    /**
+     * if set to true, then the association is negated i.e. is not true
+     * 
+     */
+    @JsonProperty("negated")
+    @JsonPropertyDescription("if set to true, then the association is negated i.e. is not true")
+    private String negated;
+    /**
+     * connects an association to the object of the association. For example, in a gene-to-phenotype association, the gene is subject and phenotype is object.
+     * 
+     */
+    @JsonProperty("object")
+    @JsonPropertyDescription("connects an association to the object of the association. For example, in a gene-to-phenotype association, the gene is subject and phenotype is object.")
+    private String object;
+    /**
+     * a qualifier used in a phenotypic association to state when the phenotype appears is in the subject
+     * 
+     */
+    @JsonProperty("onset_qualifier")
+    @JsonPropertyDescription("a qualifier used in a phenotypic association to state when the phenotype appears is in the subject")
+    private String onsetQualifier;
+    /**
+     * connects an association to the agent (person, organization or group) that provided it
+     * 
+     */
+    @JsonProperty("provided_by")
+    @JsonPropertyDescription("connects an association to the agent (person, organization or group) that provided it")
+    private String providedBy;
+    /**
+     * connects an association to publications supporting the association
+     * 
+     */
+    @JsonProperty("publications")
+    @JsonPropertyDescription("connects an association to publications supporting the association")
+    private List<String> publications = new ArrayList<String>();
+    /**
+     * connects an association to qualifiers that modify or qualify the meaning of that association
+     * 
+     */
+    @JsonProperty("qualifiers")
+    @JsonPropertyDescription("connects an association to qualifiers that modify or qualify the meaning of that association")
+    private List<String> qualifiers = new ArrayList<String>();
+    /**
+     * the relationship type by which a subject is connected to an object in an association
+     * 
+     */
+    @JsonProperty("relation")
+    @JsonPropertyDescription("the relationship type by which a subject is connected to an object in an association")
+    private String relation;
+    /**
+     * a qualifier used in a phenotypic association to state how severe the phenotype is in the subject
+     * 
+     */
+    @JsonProperty("severity_qualifier")
+    @JsonPropertyDescription("a qualifier used in a phenotypic association to state how severe the phenotype is in the subject")
+    private String severityQualifier;
+    /**
+     * a qualifier used in a phenotypic association to state whether the association is specific to a particular sex.
+     * 
+     */
+    @JsonProperty("sex_qualifier")
+    @JsonPropertyDescription("a qualifier used in a phenotypic association to state whether the association is specific to a particular sex.")
+    private String sexQualifier;
+    /**
+     * connects an association to the subject of the association. For example, in a gene-to-phenotype association, the gene is subject and phenotype is object.
+     * 
+     */
+    @JsonProperty("subject")
+    @JsonPropertyDescription("connects an association to the subject of the association. For example, in a gene-to-phenotype association, the gene is subject and phenotype is object.")
+    private String subject;
+
+    /**
+     * connects an association to the type of association (e.g. gene to phenotype)
+     * 
+     */
+    @JsonProperty("association_type")
+    public String getAssociationType() {
+        return associationType;
+    }
+
+    /**
+     * connects an association to the type of association (e.g. gene to phenotype)
+     * 
+     */
+    @JsonProperty("association_type")
+    public void setAssociationType(String associationType) {
+        this.associationType = associationType;
+    }
+
+    /**
+     * a qualifier used in a phenotypic association to state how frequent the phenotype is observed in the subject
+     * 
+     */
+    @JsonProperty("frequency_qualifier")
+    public String getFrequencyQualifier() {
+        return frequencyQualifier;
+    }
+
+    /**
+     * a qualifier used in a phenotypic association to state how frequent the phenotype is observed in the subject
+     * 
+     */
+    @JsonProperty("frequency_qualifier")
+    public void setFrequencyQualifier(String frequencyQualifier) {
+        this.frequencyQualifier = frequencyQualifier;
+    }
+
+    @JsonProperty("id")
+    public String getId() {
+        return id;
+    }
+
+    @JsonProperty("id")
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    /**
+     * A human-readable name for a thing
+     * 
+     */
+    @JsonProperty("label")
+    public String getLabel() {
+        return label;
+    }
+
+    /**
+     * A human-readable name for a thing
+     * 
+     */
+    @JsonProperty("label")
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    /**
+     * if set to true, then the association is negated i.e. is not true
+     * 
+     */
+    @JsonProperty("negated")
+    public String getNegated() {
+        return negated;
+    }
+
+    /**
+     * if set to true, then the association is negated i.e. is not true
+     * 
+     */
+    @JsonProperty("negated")
+    public void setNegated(String negated) {
+        this.negated = negated;
+    }
+
+    /**
+     * connects an association to the object of the association. For example, in a gene-to-phenotype association, the gene is subject and phenotype is object.
+     * 
+     */
+    @JsonProperty("object")
+    public String getObject() {
+        return object;
+    }
+
+    /**
+     * connects an association to the object of the association. For example, in a gene-to-phenotype association, the gene is subject and phenotype is object.
+     * 
+     */
+    @JsonProperty("object")
+    public void setObject(String object) {
+        this.object = object;
+    }
+
+    /**
+     * a qualifier used in a phenotypic association to state when the phenotype appears is in the subject
+     * 
+     */
+    @JsonProperty("onset_qualifier")
+    public String getOnsetQualifier() {
+        return onsetQualifier;
+    }
+
+    /**
+     * a qualifier used in a phenotypic association to state when the phenotype appears is in the subject
+     * 
+     */
+    @JsonProperty("onset_qualifier")
+    public void setOnsetQualifier(String onsetQualifier) {
+        this.onsetQualifier = onsetQualifier;
+    }
+
+    /**
+     * connects an association to the agent (person, organization or group) that provided it
+     * 
+     */
+    @JsonProperty("provided_by")
+    public String getProvidedBy() {
+        return providedBy;
+    }
+
+    /**
+     * connects an association to the agent (person, organization or group) that provided it
+     * 
+     */
+    @JsonProperty("provided_by")
+    public void setProvidedBy(String providedBy) {
+        this.providedBy = providedBy;
+    }
+
+    /**
+     * connects an association to publications supporting the association
+     * 
+     */
+    @JsonProperty("publications")
+    public List<String> getPublications() {
+        return publications;
+    }
+
+    /**
+     * connects an association to publications supporting the association
+     * 
+     */
+    @JsonProperty("publications")
+    public void setPublications(List<String> publications) {
+        this.publications = publications;
+    }
+
+    /**
+     * connects an association to qualifiers that modify or qualify the meaning of that association
+     * 
+     */
+    @JsonProperty("qualifiers")
+    public List<String> getQualifiers() {
+        return qualifiers;
+    }
+
+    /**
+     * connects an association to qualifiers that modify or qualify the meaning of that association
+     * 
+     */
+    @JsonProperty("qualifiers")
+    public void setQualifiers(List<String> qualifiers) {
+        this.qualifiers = qualifiers;
+    }
+
+    /**
+     * the relationship type by which a subject is connected to an object in an association
+     * 
+     */
+    @JsonProperty("relation")
+    public String getRelation() {
+        return relation;
+    }
+
+    /**
+     * the relationship type by which a subject is connected to an object in an association
+     * 
+     */
+    @JsonProperty("relation")
+    public void setRelation(String relation) {
+        this.relation = relation;
+    }
+
+    /**
+     * a qualifier used in a phenotypic association to state how severe the phenotype is in the subject
+     * 
+     */
+    @JsonProperty("severity_qualifier")
+    public String getSeverityQualifier() {
+        return severityQualifier;
+    }
+
+    /**
+     * a qualifier used in a phenotypic association to state how severe the phenotype is in the subject
+     * 
+     */
+    @JsonProperty("severity_qualifier")
+    public void setSeverityQualifier(String severityQualifier) {
+        this.severityQualifier = severityQualifier;
+    }
+
+    /**
+     * a qualifier used in a phenotypic association to state whether the association is specific to a particular sex.
+     * 
+     */
+    @JsonProperty("sex_qualifier")
+    public String getSexQualifier() {
+        return sexQualifier;
+    }
+
+    /**
+     * a qualifier used in a phenotypic association to state whether the association is specific to a particular sex.
+     * 
+     */
+    @JsonProperty("sex_qualifier")
+    public void setSexQualifier(String sexQualifier) {
+        this.sexQualifier = sexQualifier;
+    }
+
+    /**
+     * connects an association to the subject of the association. For example, in a gene-to-phenotype association, the gene is subject and phenotype is object.
+     * 
+     */
+    @JsonProperty("subject")
+    public String getSubject() {
+        return subject;
+    }
+
+    /**
+     * connects an association to the subject of the association. For example, in a gene-to-phenotype association, the gene is subject and phenotype is object.
+     * 
+     */
+    @JsonProperty("subject")
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this).append("associationType", associationType).append("frequencyQualifier", frequencyQualifier).append("id", id).append("label", label).append("negated", negated).append("object", object).append("onsetQualifier", onsetQualifier).append("providedBy", providedBy).append("publications", publications).append("qualifiers", qualifiers).append("relation", relation).append("severityQualifier", severityQualifier).append("sexQualifier", sexQualifier).append("subject", subject).toString();
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder().append(providedBy).append(sexQualifier).append(negated).append(subject).append(associationType).append(frequencyQualifier).append(qualifiers).append(label).append(relation).append(onsetQualifier).append(severityQualifier).append(id).append(object).append(publications).toHashCode();
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+        if ((other instanceof VariantToPhenotypicFeatureAssociation) == false) {
+            return false;
+        }
+        VariantToPhenotypicFeatureAssociation rhs = ((VariantToPhenotypicFeatureAssociation) other);
+        return new EqualsBuilder().append(providedBy, rhs.providedBy).append(sexQualifier, rhs.sexQualifier).append(negated, rhs.negated).append(subject, rhs.subject).append(associationType, rhs.associationType).append(frequencyQualifier, rhs.frequencyQualifier).append(qualifiers, rhs.qualifiers).append(label, rhs.label).append(relation, rhs.relation).append(onsetQualifier, rhs.onsetQualifier).append(severityQualifier, rhs.severityQualifier).append(id, rhs.id).append(object, rhs.object).append(publications, rhs.publications).isEquals();
+    }
+
+}
