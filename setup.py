1fc3879b49fdb5c7088679abd03ee9f0cea5c0a4
Deepak Unni <deepak.unni3@gmail.com>
2019-06-28 12:56:42 -0700

    Revert "Update setup.py"
    This reverts commit 81701e195b6f67621f0c53d89ef1c90aebc14bf0.

--- setup.py
+++ setup.py
@@ -1,42 +1,8 @@
+#!/usr/bin/env python
+
 from setuptools import setup
 
-
-NAME = 'biolink_model'
-DESCRIPTION = 'A high level datamodel of biological entities and associations'
-URL = 'https://github.com/biolink/biolink-model'
-AUTHOR = 'Harold Solbrig'
-EMAIL = 'solbrig@jhu.edu'
-REQUIRES_PYTHON = '>=3.7'
-VERSION = '0.0.1'
-LICENSE = 'CC0 1.0 Universal'
-
-REQUIRED = [
-    "biolinkml>=1.3.8"
-]
-
-EXTRAS = {}
-
-
 setup(
-    name=NAME,
-    version=VERSION,
-    author=AUTHOR,
-    author_email=EMAIL,
-    python_requires=REQUIRES_PYTHON,
-    url=URL,
-    description=DESCRIPTION,
-    long_description=open('README.md').read(),
-    license=LICENSE,
-    packages=['biolink'],
-    keywords='BioLink Datamodel NCATS NCATS-Translator',
-    classifiers=[
-        'Intended Audience :: Developers'
-        'Intended Audience :: Science/Research',
-        'Intended Audience :: Healthcare Industry',
-        'Topic :: Scientific/Engineering :: Bio-informatics',
-        'License :: CC0 1.0 Universal (CC0 1.0)',
-        'Programming Language :: Python :: 3'
-    ],
-    install_requires=REQUIRED,
-    extras_require=EXTRAS
+    setup_requires=['pbr'],
+    pbr=True,
 )
