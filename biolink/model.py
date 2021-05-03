d65c47c848c5eeddf332c11ff2feb148536f95f4
Sierra Taylor Moxon <sierra.taylor@gmail.com>
2021-05-03 10:57:25 -0700

    remove BFO:0000068 and BFO:0000069 from the model. fixes 696

--- biolink/model.py
+++ biolink/model.py
@@ -1,5 +1,5 @@
 # Auto generated from biolink-model.yaml by pythongen.py version: 0.9.0
-# Generation date: 2021-04-27 14:41
+# Generation date: 2021-05-03 10:17
 # Schema: Biolink-Model
 #
 # id: https://w3id.org/biolink/biolink-model
