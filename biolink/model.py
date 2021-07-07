615629a4e3ad198b68c3ef08c4e8935816c7deb3
Deepak Unni <deepak.unni3@gmail.com>
2021-07-07 01:41:01 +0000

    Regenerate artifacts from biolink-model.yaml

--- biolink/model.py
+++ biolink/model.py
@@ -1,5 +1,5 @@
 # Auto generated from biolink-model.yaml by pythongen.py version: 0.9.0
-# Generation date: 2021-07-06 18:21
+# Generation date: 2021-07-07 01:36
 # Schema: Biolink-Model
 #
 # id: https://w3id.org/biolink/biolink-model
