15b159e7528b5f3ce4cfe9e8753313f2a95340e8
GitHub Action <action@github.com>
2022-03-24 02:14:22 +0000

    Regenerate artifacts from biolink-model.yaml

--- biolink/model.py
+++ biolink/model.py
@@ -1,5 +1,5 @@
 # Auto generated from biolink-model.yaml by pythongen.py version: 0.9.0
-# Generation date: 2022-03-24T00:50:34
+# Generation date: 2022-03-24T02:12:44
 # Schema: Biolink-Model
 #
 # id: https://w3id.org/biolink/biolink-model
@@ -26,7 +26,7 @@ from linkml_runtime.linkml_model.types import Boolean, Date, Double, Float, Inte
 from linkml_runtime.utils.metamodelcore import Bool, URIorCURIE, XSDDate, XSDTime
 
 metamodel_version = "1.7.0"
-version = "2.2.15"
+version = "2.2.16"
 
 # Overwrite dataclasses _init_fn to add **kwargs in __init__
 dataclasses._init_fn = dataclasses_init_fn_with_kwargs
