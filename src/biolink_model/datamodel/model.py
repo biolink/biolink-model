31a7e590742c75dbd4047a6789832ecf6958a084
Sierra Taylor Moxon <sierra.taylor@gmail.com>
2023-11-29 12:48:13 -0800

    lintimg

--- src/biolink_model/datamodel/model.py
+++ src/biolink_model/datamodel/model.py
@@ -1,5 +1,5 @@
 # Auto generated from biolink_model.yaml by pythongen.py version: 0.0.1
-# Generation date: 2023-11-29T11:58:56
+# Generation date: 2023-11-29T12:45:47
 # Schema: Biolink-Model
 #
 # id: https://w3id.org/biolink/biolink-model
--- src/biolink_model/scripts/classprefixes.py
+++ src/biolink_model/scripts/classprefixes.py
@@ -1,5 +1,5 @@
 # Auto generated from class_prefixes.yaml by pythongen.py version: 0.0.1
-# Generation date: 2023-11-29T12:00:42
+# Generation date: 2023-11-29T12:47:36
 # Schema: BiolinkClassPrefixes
 #
 # id: biolink-model-class-prefixes
