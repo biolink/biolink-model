69d46a757325624b9305510d247718fa5367c4c6
Sierra Taylor Moxon <sierra.taylor@gmail.com>
2023-11-29 10:54:08 -0800

    fix up failing urls

--- biolink/model.py
+++ biolink/model.py
@@ -1,5 +1,5 @@
 # Auto generated from biolink-model.yaml by pythongen.py version: 0.0.1
-# Generation date: 2023-11-29T09:06:14
+# Generation date: 2023-11-29T09:19:36
 # Schema: Biolink-Model
 #
 # id: https://w3id.org/biolink/biolink-model
