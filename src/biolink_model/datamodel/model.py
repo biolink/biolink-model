b5ec2e22345a17de5941239d5fb04b28021818d7
Sierra Taylor Moxon <sierra.taylor@gmail.com>
2023-12-11 15:52:22 -0800

    fix up yaml - lint

--- src/biolink_model/datamodel/model.py
+++ src/biolink_model/datamodel/model.py
@@ -1,5 +1,5 @@
 # Auto generated from biolink_model.yaml by pythongen.py version: 0.0.1
-# Generation date: 2023-12-11T15:45:54
+# Generation date: 2023-12-11T15:50:31
 # Schema: Biolink-Model
 #
 # id: https://w3id.org/biolink/biolink-model
