569ecf63ae59bfd200dda8dd871ed50c2dff4345
GitHub Action <action@github.com>
2024-01-11 21:16:48 +0000

    Regenerate artifacts from biolink-model.yaml

--- src/biolink_model/datamodel/model.py
+++ src/biolink_model/datamodel/model.py
@@ -1,5 +1,5 @@
 # Auto generated from biolink_model.yaml by pythongen.py version: 0.0.1
-# Generation date: 2024-01-09T01:12:11
+# Generation date: 2024-01-11T21:14:32
 # Schema: Biolink-Model
 #
 # id: https://w3id.org/biolink/biolink-model
