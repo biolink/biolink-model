6fd28efdd806e38c30e73eae30dd2ef82d748be5
GitHub Action <action@github.com>
2024-04-12 22:31:57 +0000

    Regenerate artifacts from biolink-model.yaml

--- src/biolink_model/datamodel/model.py
+++ src/biolink_model/datamodel/model.py
@@ -1,5 +1,5 @@
 # Auto generated from biolink_model.yaml by pythongen.py version: 0.0.1
-# Generation date: 2024-04-12T15:18:12
+# Generation date: 2024-04-12T22:30:57
 # Schema: Biolink-Model
 #
 # id: https://w3id.org/biolink/biolink-model
--- src/biolink_model/scripts/classprefixes.py
+++ src/biolink_model/scripts/classprefixes.py
@@ -1,5 +1,5 @@
 # Auto generated from class_prefixes.yaml by pythongen.py version: 0.0.1
-# Generation date: 2024-04-12T15:19:05
+# Generation date: 2024-04-12T22:31:49
 # Schema: BiolinkClassPrefixes
 #
 # id: biolink-model-class-prefixes
