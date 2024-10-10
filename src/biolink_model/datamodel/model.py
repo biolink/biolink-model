640bd45bb8c3ad7a08821a2dc9a4f28893d406bf
Sierra Taylor Moxon <sierra.taylor@gmail.com>
2024-10-10 09:34:26 -0700

    regenerate classprefixes files

--- src/biolink_model/datamodel/model.py
+++ src/biolink_model/datamodel/model.py
@@ -1,5 +1,5 @@
 # Auto generated from biolink_model.yaml by pythongen.py version: 0.0.1
-# Generation date: 2024-10-10T09:30:56
+# Generation date: 2024-10-10T09:32:55
 # Schema: Biolink-Model
 #
 # id: https://w3id.org/biolink/biolink-model
