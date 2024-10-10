ab0bc0bf3e5c84eb850483fa8bff0ac98f57f980
Sierra Taylor Moxon <sierra.taylor@gmail.com>
2024-10-10 09:07:54 -0700

    regenerate artifacts with correct pydantic call

--- src/biolink_model/datamodel/model.py
+++ src/biolink_model/datamodel/model.py
@@ -1,5 +1,5 @@
 # Auto generated from biolink_model.yaml by pythongen.py version: 0.0.1
-# Generation date: 2024-10-10T08:29:34
+# Generation date: 2024-10-10T09:05:31
 # Schema: Biolink-Model
 #
 # id: https://w3id.org/biolink/biolink-model
