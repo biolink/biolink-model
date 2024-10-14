99d28ac9a50df655545b51bbb6e265de394fc7f7
Sierra Taylor Moxon <sierra.taylor@gmail.com>
2024-10-14 15:41:24 -0700

    fix spacing

--- src/biolink_model/scripts/classprefixes.py
+++ src/biolink_model/scripts/classprefixes.py
@@ -1,5 +1,5 @@
 # Auto generated from class_prefixes.yaml by pythongen.py version: 0.0.1
-# Generation date: 2024-10-11T00:00:49
+# Generation date: 2024-10-14T15:39:36
 # Schema: BiolinkClassPrefixes
 #
 # id: biolink-model-class-prefixes
