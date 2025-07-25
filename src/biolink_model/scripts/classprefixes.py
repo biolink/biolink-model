2401d0cfb2112171fcb7d226ee4eb29f46914f36
RichardBruskiewich <richard.bruskiewich@delphinai.com>
2025-07-25 16:40:06 -0700

    Regenerated artifacts for inline-sources-on-association branch (still in PR to main); make test has one error (not diagnosed)

--- src/biolink_model/scripts/classprefixes.py
+++ src/biolink_model/scripts/classprefixes.py
@@ -1,5 +1,5 @@
 # Auto generated from class_prefixes.yaml by pythongen.py version: 0.0.1
-# Generation date: 2025-07-25T22:58:14
+# Generation date: 2025-07-25T16:36:42
 # Schema: BiolinkClassPrefixes
 #
 # id: biolink-model-class-prefixes
