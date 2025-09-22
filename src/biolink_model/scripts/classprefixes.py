64f644c1d2f627dd3dfe3a13b5263f782ae8adac
Sierra Taylor Moxon <sierra.taylor@gmail.com>
2025-09-22 10:35:05 -0700

    regenerate

--- src/biolink_model/scripts/classprefixes.py
+++ src/biolink_model/scripts/classprefixes.py
@@ -1,5 +1,5 @@
 # Auto generated from class_prefixes.yaml by pythongen.py version: 0.0.1
-# Generation date: 2025-09-08T16:57:00
+# Generation date: 2025-09-10T14:07:32
 # Schema: BiolinkClassPrefixes
 #
 # id: biolink-model-class-prefixes
