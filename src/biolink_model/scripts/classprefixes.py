1b65128087eb0560b194a6869820c78565f23266
Parker Gleason-Bremer <pgleasonbremer@altoslabs.com>
2025-10-25 08:35:25 -0700

    generated code

--- src/biolink_model/scripts/classprefixes.py
+++ src/biolink_model/scripts/classprefixes.py
@@ -1,5 +1,5 @@
 # Auto generated from class_prefixes.yaml by pythongen.py version: 0.0.1
-# Generation date: 2025-10-22T04:18:47
+# Generation date: 2025-10-25T08:32:06
 # Schema: BiolinkClassPrefixes
 #
 # id: biolink-model-class-prefixes
