a9d817497de8cce3a39eb64b4818920ea67fa71f
Sierra Taylor Moxon <sierra.taylor@gmail.com>
2025-11-14 13:55:31 -0800

    remove circular imports annotation

--- src/biolink_model/datamodel/model.py
+++ src/biolink_model/datamodel/model.py
@@ -1,5 +1,5 @@
 # Auto generated from biolink_model.yaml by pythongen.py version: 0.0.1
-# Generation date: 2025-11-14T21:30:56
+# Generation date: 2025-11-14T13:45:28
 # Schema: Biolink-Model
 #
 # id: https://w3id.org/biolink/vocab/
--- src/biolink_model/scripts/classprefixes.py
+++ src/biolink_model/scripts/classprefixes.py
@@ -1,5 +1,5 @@
 # Auto generated from class_prefixes.yaml by pythongen.py version: 0.0.1
-# Generation date: 2025-11-14T21:31:46
+# Generation date: 2025-11-14T13:46:54
 # Schema: BiolinkClassPrefixes
 #
 # id: biolink-model-class-prefixes
