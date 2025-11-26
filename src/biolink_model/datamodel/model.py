ff786107ebbb564a1a413130140eee7e713c104d
Sierra Taylor Moxon <sierra.taylor@gmail.com>
2025-11-26 10:00:02 -0800

    add some attempts at warning supression

--- src/biolink_model/datamodel/model.py
+++ src/biolink_model/datamodel/model.py
@@ -1,5 +1,5 @@
 # Auto generated from biolink_model.yaml by pythongen.py version: 0.0.1
-# Generation date: 2025-11-26T09:49:41
+# Generation date: 2025-11-26T09:56:00
 # Schema: Biolink-Model
 #
 # id: https://w3id.org/biolink/vocab/
