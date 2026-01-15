438ca66e8ecc9a1d0b8b83ecaaae7464142bc1e3
RichardBruskiewich <richard.bruskiewich@delphinai.com>
2026-01-14 21:20:11 -0800

    artifact regenerated for biolink:has_evidence model tweaks

--- src/biolink_model/datamodel/model.py
+++ src/biolink_model/datamodel/model.py
@@ -1,5 +1,5 @@
 # Auto generated from biolink_model.yaml by pythongen.py version: 0.0.1
-# Generation date: 2026-01-15T02:38:07
+# Generation date: 2026-01-14T21:15:26
 # Schema: Biolink-Model
 #
 # id: https://w3id.org/biolink/vocab/
--- src/biolink_model/scripts/classprefixes.py
+++ src/biolink_model/scripts/classprefixes.py
@@ -1,5 +1,5 @@
 # Auto generated from class_prefixes.yaml by pythongen.py version: 0.0.1
-# Generation date: 2026-01-15T02:39:05
+# Generation date: 2026-01-14T21:16:13
 # Schema: BiolinkClassPrefixes
 #
 # id: biolink-model-class-prefixes
