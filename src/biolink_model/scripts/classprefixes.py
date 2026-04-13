380e7d53a2bd1c41f263d7625fe8d155ffa74d63
Sierra Taylor Moxon <sierra.taylor@gmail.com>
2026-04-13 13:31:08 -0700

    regenerate artifacts

--- src/biolink_model/scripts/classprefixes.py
+++ src/biolink_model/scripts/classprefixes.py
@@ -1,5 +1,5 @@
 # Auto generated from class_prefixes.yaml by pythongen.py version: 0.0.1
-# Generation date: 2026-04-13T18:24:41
+# Generation date: 2026-04-13T12:15:08
 # Schema: BiolinkClassPrefixes
 #
 # id: biolink-model-class-prefixes
