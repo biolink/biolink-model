6aba8f4a701eb9944fafadf3342c79990ce70ca7
Sierra Taylor Moxon <sierra.taylor@gmail.com>
2026-04-21 10:19:14 -0700

    add descriptions to slots

--- src/biolink_model/datamodel/model.py
+++ src/biolink_model/datamodel/model.py
@@ -1,5 +1,5 @@
 # Auto generated from biolink_model.yaml by pythongen.py version: 0.0.1
-# Generation date: 2026-04-20T17:50:51
+# Generation date: 2026-04-21T10:15:56
 # Schema: Biolink-Model
 #
 # id: https://w3id.org/biolink/vocab/
