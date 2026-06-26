845ce2a43fcc9d9a2db85b3b3d667f92a5722bee
Sierra Taylor Moxon <sierra.taylor@gmail.com>
2026-06-26 16:56:52 -0700

    fix linting issues

--- src/biolink_model/datamodel/model.py
+++ src/biolink_model/datamodel/model.py
@@ -1,5 +1,5 @@
 # Auto generated from biolink_model.yaml by pythongen.py version: 0.0.1
-# Generation date: 2026-06-26T22:43:46
+# Generation date: 2026-06-26T16:53:10
 # Schema: Biolink-Model
 #
 # id: https://w3id.org/biolink/vocab/
--- src/biolink_model/scripts/classprefixes.py
+++ src/biolink_model/scripts/classprefixes.py
@@ -1,5 +1,5 @@
 # Auto generated from class_prefixes.yaml by pythongen.py version: 0.0.1
-# Generation date: 2026-06-26T22:44:33
+# Generation date: 2026-06-26T16:54:59
 # Schema: BiolinkClassPrefixes
 #
 # id: biolink-model-class-prefixes
