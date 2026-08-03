aed9ea95a1e63873af0bd9109f2442eb5b555dd8
Sierra Taylor Moxon <sierra.taylor@gmail.com>
2026-08-03 08:35:32 -0700

    fix attributes, add to associations

--- src/biolink_model/scripts/classprefixes.py
+++ src/biolink_model/scripts/classprefixes.py
@@ -1,5 +1,5 @@
 # Auto generated from class_prefixes.yaml by pythongen.py version: 0.0.1
-# Generation date: 2026-06-27T00:45:26
+# Generation date: 2026-08-03T08:33:47
 # Schema: BiolinkClassPrefixes
 #
 # id: biolink-model-class-prefixes
