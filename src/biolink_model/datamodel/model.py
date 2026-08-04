cb2bea26b67138f20bd75dc8ac92ebdc69bdc732
Sierra Taylor Moxon <sierra.taylor@gmail.com>
2026-08-04 11:33:13 -0700

    regenerate artifacts

--- src/biolink_model/datamodel/model.py
+++ src/biolink_model/datamodel/model.py
@@ -1,5 +1,5 @@
 # Auto generated from biolink_model.yaml by pythongen.py version: 0.0.1
-# Generation date: 2026-08-04T11:25:12
+# Generation date: 2026-08-04T11:31:12
 # Schema: Biolink-Model
 #
 # id: https://w3id.org/biolink/vocab/
